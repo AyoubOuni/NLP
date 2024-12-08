3a3f7381a856c5ab2f44c6133a98812520e0ae26
Ayoub Ouni <ayoubelouni6@gmail.com>
2024-12-08 23:05:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -392,8 +392,4 @@ def check_items():
 
 
 if __name__ == "__main__":
-    print("Starting Flask server...")
-    # Get the port from the environment variable, default to 5000 if not set
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
-    app.run(debug=True, host="0.0.0.0", port=5000)
-    print("Flask server stopped.")
+    app.run(debug=True)
